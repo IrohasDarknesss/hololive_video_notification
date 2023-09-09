a044436d975ffb6a91cde7bfc8099037d98547d8
IrohasDarknesss <kaine0401mario@gmail.com>
2023-09-10 01:43:19 +0900

    setting API info

--- youtubeAPi.rb
+++ youtubeAPi.rb
@@ -5,3 +5,13 @@ require 'google/apis/YoutubeV3'
 youtube = Google::Apis::YoutubeV3::YouTubeService.new
 youtube.key =  File.open('./config/Auth.cfg')
 
+# Get info
+channel_ids = File.readlines('channel_ids.txt').map(&:chomp)
+
+#Create CSV
+CSV.open('Streaming_data.csv', "wb") do |csv|
+    csv << ['Channel Name', 'Video URL']
+    channel_ids.each do |channel_id|
+        res = Youtube.list_channel_activities(
+            part:'snippet', channel_id:channel_id,max_result: 3
+        )
